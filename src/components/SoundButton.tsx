4b9a84d048cb6e1718039e5d3bd9dec3d83d1ed2
Brent Luna <brentadamluna@gmail.com>
2019-03-02 12:12:03 -0800

    Initial test of button playing clip works, need to now make a list component to map out buttons for for every clip

--- src/components/SoundButton.tsx
+++ src/components/SoundButton.tsx
@@ -1,12 +1,17 @@
 import React, {Component, useState} from 'react';
 import { Button, Text, View } from 'react-native';
+import Sound from 'react-native-sound';
+const clip = require('./../../assets/tbj_soundclips/DemarcusCousins12-Inch.mp3');
 
 export default function SoundButton({}) {
   const [count, setCount] = useState(0)
   const handlePress = () => setCount(count + 1)
+  const sound: any = new Sound(clip, error => console.log(error, sound));
+
   return(
     <View>
-      <Button onPress={handlePress} title='test'>Test</Button>
+      <Button onPress={() => sound.play((success: Object) => console.log(success)
+      )} title='test'>Test</Button>
       <Text>{count}</Text>
     </View>
   )
